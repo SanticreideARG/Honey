382d71fb57a1083f215711562d72e68514ddd55c
Santiago Creide <santi.creide@gmail.com>
2022-10-29 02:28:45 -0300

    carritoLogo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,15 @@
+import React from 'react'
 import "./App.css";
 import Navbar from "./Navbar/Navbar";
 import Home from "./Home/Home";
-
+import ItemListContainer from "./ItemListContainer/ItemListContainer";
 
 const App = () => {
   return (
     <>
      <Navbar/>
      <Home/> 
+     <ItemListContainer />
     </>
    
   );
--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import cart from './img/cart.png';
 
 const CartWidget = () => {
     return (
         <>
+          <img className="navbar-logo" src={cart} alt="" />
+
         </>
     );
 };
